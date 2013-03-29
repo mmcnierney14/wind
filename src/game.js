46e7d5b010c22e87dac7620857eeb2dc19d6af62
Matthew McNierney <mmcnierney14@gmail.com>
2013-03-29 10:58:36 -0400

    wip apply wind force on click

--- src/game.js
+++ src/game.js
@@ -6,10 +6,17 @@ var gravity = new b2Vec2(0, 0);
 var doSleep = true;
 var world = new b2World(worldAABB, gravity, doSleep);
 
+// Global reference to bodies
+var bodies = {};
+
 function createBox(world, x, y, width, height, fixed) {
 	if (typeof(fixed) == 'undefined') fixed = true;
 	var boxSd = new b2BoxDef();
-	if (!fixed) boxSd.density = 1.0;
+	if (!fixed) {
+    boxSd.density = 1.0;
+    boxSd.friction = 0.5;
+    boxSd.restitution = 0.2;
+  }
 	boxSd.extents.Set(width, height);
 	var boxBd = new b2BodyDef();
 	boxBd.AddShape(boxSd);
@@ -28,6 +35,7 @@ Crafty.c("Boat", {
     
     // Create box2d box to handle physics
     var boat_box = createBox(world, this.x, this.y, this.w, this.h, false);
+    bodies["boat"] = boat_box;
     
     this.bind("EnterFrame", function() {
       position = boat_box.m_position;
@@ -39,17 +47,24 @@ Crafty.c("Boat", {
   }
 });
 
-Crafty.c("physics_world", {
+Crafty.c("PhysicsWorld", {
   timeStep: 1.0/60,
   iteration: 1,
   init: function() {
-    this.requires("2D");
-    
-    this.visible = false;
+    this.requires("2D, Canvas, Mouse");
+    this.attr({w: 600, h: 500});
     
     // Step the world each frame
     this.bind("EnterFrame", function() {
     	world.Step(this.timeStep, this.iteration);
+    });
+    
+    // Create wind vector on click
+    this.bind("MouseDown", function(event) {
+      console.log("mouse x: " + event.x + ", mouse y: " + event.y);
+      boat = bodies["boat"];
+      force = new b2Vec2(1000, 1000);
+      boat.ApplyForce(force, boat.GetCenterPosition());
     });
   }
 });
@@ -58,7 +73,7 @@ Crafty.c("physics_world", {
 Crafty.scene("main", function() {
   
   // Create the boat entity
-  var world = Crafty.e("physics_world");
+  var world = Crafty.e("PhysicsWorld");
   var boat = Crafty.e("Boat");
     
 });
